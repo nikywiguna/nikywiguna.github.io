340c6737fea1c4aef9ecce986db9dc5d372a92bb
JovHariono <128022304+JovHariono@users.noreply.github.com>
2023-07-07 13:24:37 +0700

    bisa yok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import '../styles/style.css'
 import '../styles/style2.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
